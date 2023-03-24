c4f145f4680264ccf74091f6dd1eaaf58cc744f4
Mezie-Gift <iammeziegift@gmail.com>
2023-03-24 15:35:04 -0700

    process kill

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# This manifest kills a process called `Killmenow`
+
+$procx_name = 'Killmenow'
+exec {"kill_${procx_name}":
+  command => "/usr/bin/pkill ${procx_name}",
+  onlyif => "/usr/bin/pgrep ${procx_name}",
+}
