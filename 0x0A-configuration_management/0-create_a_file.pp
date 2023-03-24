f5e78dba95df4ea321a0c51cd5443847488ea79b
Mezie-Gift <iammeziegift@gmail.com>
2023-03-24 13:59:50 -0700

    Create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Create file with content
 
-file { '/tmp':
+file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet'
+  content => 'I love Puppet'
 }
