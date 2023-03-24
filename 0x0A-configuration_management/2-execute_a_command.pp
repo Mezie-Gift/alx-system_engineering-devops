6f61a6bac8b2af935d7dc780f163162ecb6f04a0
Mezie-Gift <iammeziegift@gmail.com>
2023-03-24 15:37:16 -0700

    process kill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -3,5 +3,5 @@
 $procx_name = 'Killmenow'
 exec {"kill_${procx_name}":
   command => "/usr/bin/pkill ${procx_name}",
-  onlyif => "/usr/bin/pgrep ${procx_name}",
+  onlyif  => "/usr/bin/pgrep ${procx_name}",
 }
