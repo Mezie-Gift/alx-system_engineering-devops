d1a4f6aaae16c4676f81190eaf0b2119ce332b39
Mezie-Gift <iammeziegift@gmail.com>
2023-08-14 23:09:24 +0000

    puppet script

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# This puppet script 'adjust' the ulimit settings for the nginx process.
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
